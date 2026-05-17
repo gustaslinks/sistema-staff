e73bd1371e3348a89791e99edfe42a812dedc4aa
gustaslinks <gustaslinks@gmail.com>
2026-05-16 23:37:24 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,9 @@
-const CACHE_VERSION = 'sistema-staff-v4.2';
+const CACHE_NAME = 'rcp-run-staff-v4-3';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) => Promise.all(
-      keys.filter((key) => key !== CACHE_VERSION).map((key) => caches.delete(key))
-    )).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', () => {
-  // Sem cache agressivo nesta versão para evitar arquivo antigo no GitHub Pages.
+  event.waitUntil(clients.claim());
 });
